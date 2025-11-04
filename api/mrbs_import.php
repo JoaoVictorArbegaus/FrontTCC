4b956105577e3ba2e06f6fef9de4976d6975d15b
lucasSanto5 <lucasdossantosborges382@gmail.com>
2025-11-03 22:37:30 -0300

    mapeamento correto para períodos noturnos das salas de aula

--- api/mrbs_import.php
+++ api/mrbs_import.php
@@ -140,7 +140,7 @@ $mapFrontToMrbs = [
   4=>5, 5=>6, 6=>7, 7=>8,
   8=>10, 9=>11, 10=>12, 11=>13
 ];
-$validMrbsIndex = [0,1,2,3,5,6,7,8,10,11,12,13];
+$validMrbsIndex = [0,1,2,3,5,6,7,8,10,13,15,16];
 $validToPos = array_values($validMrbsIndex);
 
 // Técnica do “meio-dia”: minuto = índice do período
